ce7553f29db54258a6eee2906860b308c6d9fcc2
Martin Thielecke <github@mthie.com>
2013-10-04 15:19:23 +0000

    check for a valid login now, otherwise redirect to the login page

--- cloudkeys/Overview/Handler.php
+++ cloudkeys/Overview/Handler.php
@@ -3,6 +3,17 @@
 class OverviewHandler extends BaseHttpHandler {
 
   public function get($params) {
+    $userfile = $this->session->get('username');
+    if($userfile == "") {
+      $this->response->redirect('login');
+      return;
+    }
+    try {
+      S3::getObject($this->config->get('AWSS3Bucket'), $userfile);
+    } catch(Exception $e) {
+      $this->response->redirect('login');
+      return;
+    }
     $this->response->display('overview');
   }
 
