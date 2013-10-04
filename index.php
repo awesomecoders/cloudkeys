47b8c8d324b330f241c7405ac4af450a1c7a479b
Martin Thielecke <github@mthie.com>
2013-10-04 10:21:32 +0000

    look for local settings file and use it, when it's available

--- index.php
+++ index.php
@@ -4,10 +4,17 @@ require_once 'lib/framework/dispatcher.php';
 
 // BaseAutoLoader::register_base_lib('mysql');
 // BaseAutoLoader::register_base_lib('couchdb');
-// BaseAutoLoader::register_base_lib('cloudcontrol');
+BaseAutoLoader::register_base_lib('cloudcontrol');
+
+if(file_exists('cloudkeys/settings_local.ini')) {
+  $config = new ConfigIni('cloudkeys/settings.ini', 'cloudkeys/settings_local.ini');
+} else {
+  $config = new ConfigIni('cloudkeys/settings.ini', null);
+}
 
 $dispatcher = new Dispatcher(
-    new ConfigIni('cloudkeys/settings.ini', null)
+    $config
   , 'cloudkeys'
 );
+
 $dispatcher->dispatch($_SERVER['REQUEST_URI']);
