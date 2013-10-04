de50de9592682f2f8ad7f21e7e66c7a1ef91df2b
Martin Thielecke <github@mthie.com>
2013-10-04 23:37:32 +0000

    get and write user database

--- cloudkeys/Ajax/Handler.php
+++ cloudkeys/Ajax/Handler.php
@@ -4,23 +4,72 @@ class AjaxHandler extends BaseHttpHandler {
 
   public function get($params) {
     $this->response->header("Content-Type", "text/json;charset=utf-8");
-    $userfile = $this->session->get('username');
-    if($userfile == "") {
-      echo json_encode(array('error' => true));
-      return;
-    }
-
-    $userdata = null;
-    try {
-      $userdata = S3::getObject($this->config->get('AWSS3Bucket'), $userfile);
-    } catch(Exception $e) {
-      echo json_encode(array('error' => true));
-      return;
-    }
-
+    $userdata = $this->checkLogin();
     if($userdata !== null) {
       $data = json_decode($userdata->body, true);
-      echo json_encode($data['data']);
+      echo json_encode(array('version' => $data['metadata']['version'], 'data' => $data['data']));
+    } else {
+      echo json_encode(array('error' => true));
+    }
+  }
+
+  public function post($params) {
+    $this->response->header("Content-Type", "text/json;charset=utf-8");
+
+    $userfile = $this->getUserfile();
+    if($userfile === null) {
+      echo json_encode(array('error' => true, 'type' => 'login'));
+      return;
+    }
+
+    $userdata = $this->checkLogin();
+    if($userdata === null) {
+      echo json_encode(array('error' => true, 'type' => 'register'));
+      return;
+    }
+
+    $data = json_decode($userdata->body, true);
+    
+    if($data['metadata']['version'] != $this->request->get('version')) {
+      echo json_encode(array('error' => true, 'type' => 'version'));
+      return;
+    }
+
+    if($this->request->get('checksum') != sha1($this->request->get('data'))) {
+      echo json_encode(array('error' => true, 'type' => 'checksum'));
+      return;
+    }
+
+    try {
+      S3::copyObject($this->config->get('AWSS3Bucket'), $userfile, $this->config->get('AWSS3Bucket'), $userfile . '.' . idate('U') . '.backup');
+      $data['metadata']['version'] = $this->request->get('checksum');
+      $data['data'] = $this->request->get('data');
+      S3::putObject(json_encode($data), $this->config->get('AWSS3Bucket'), $userfile);
+    } catch(Exception $e) {
+      echo json_encode(array('error' => true, 'message' => $e->getMessage()));
+      return;
+    }
+    echo json_encode(array('version' => $data['metadata']['version'], 'data' => $data['data']));
+  }
+
+  private function getUserfile() {
+    $userfile = $this->session->get('username');
+    if($userfile == "") {
+      return null;
+    }
+    return $userfile;
+  }
+
+  private function checkLogin() {
+    $userfile = $this->getUserfile();
+    if($userfile === null) {
+      return null;
+    }
+
+    try {
+      return S3::getObject($this->config->get('AWSS3Bucket'), $userfile);
+    } catch(Exception $e) {
+      return null;
     }
   }
 }
