82fd6cc55753bb1fab7f8d40032a6a16b1340e0d
Martin Thielecke <github@mthie.com>
2013-10-04 15:18:17 +0000

    registration page

---
+++ cloudkeys/Register/Handler.php
@@ -0,0 +1,41 @@
+<?php
+
+class RegisterHandler extends BaseHttpHandler {
+
+  public function get($params) {
+    $this->response->display('register');
+  }
+
+  public function post($params) {
+    $username = $this->request->get('username');
+    $password = $this->request->get('password');
+    if($username != ""
+      && $password != ""
+      && $password == $this->request->get('password_repeat')) {
+        if(!$this->userExists($username)) {
+          $doc = array('metadata' => array(), 'data' => array());
+          $doc['metadata']['password'] = sha1($this->config->get('passwordSalt') . $password);
+          S3::putObject(json_encode($doc), $this->config->get('AWSS3Bucket'), $this->createUserFilename($username), S3::ACL_PRIVATE);
+          $this->response->set('created', true);
+        } else {
+          $this->response->set('exists', true);
+        }
+    }
+    $this->response->display('register');
+  }
+
+  private function createUserFilename($username) {
+    return sha1($this->config->get('usernameSalt') . $username);
+  }
+
+  private function userExists($username) {
+    try {
+      $existing = S3::getObject($this->config->get('AWSS3Bucket'), $this->createUserFilename($username));
+    } catch(Exception $e) {
+      if(stristr($e->getMessage(), "[NoSuchKey]")) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
