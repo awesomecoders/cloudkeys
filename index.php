156add2d7197db64986ca6ef2e83a948ab131246
Martin Thielecke <github@mthie.com>
2013-10-04 12:43:45 +0000

    use the s3 class

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ require_once 'lib/framework/dispatcher.php';
 // BaseAutoLoader::register_base_lib('mysql');
 // BaseAutoLoader::register_base_lib('couchdb');
 BaseAutoLoader::register_base_lib('cloudcontrol');
+BaseAutoLoader::register_library_path(dirname(__FILE__) . '/lib/s3');
 
 if(file_exists('cloudkeys/settings_local.ini')) {
   $config = new ConfigIni('cloudkeys/settings.ini', 'cloudkeys/settings_local.ini');
 } else {
   $config = new ConfigIni('cloudkeys/settings.ini', null);
 }
+S3::setAuth($config->get('AWSAccessKey'), $config->get('AWSSecretKey'));
 
 $dispatcher = new Dispatcher(
     $config
